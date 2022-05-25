894354793c70a9dca626fb3ddfed34770b157d92
MOPSKATER <josua.w@gmx.de>
2022-05-25 21:23:11 +0200

    Initial commit

---
+++ src/main/java/mopskater/muse/Muse.java
@@ -0,0 +1,16 @@
+package mopskater.muse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Muse extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
